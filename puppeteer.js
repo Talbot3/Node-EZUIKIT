51294df9cd149da975765724a9b744ce81c63377
wangcheng <wangcheng@deepblueai.com>
2020-06-01 15:07:02 +0800

    refactor: clean

--- puppeteer.js
+++ puppeteer.js
@@ -5,22 +5,18 @@ const path = require('path');
 (async () => {
     const browser = await puppeteer.launch({headless: false , args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
-    await page.goto('http://localhost:8000/ui_voice.html', {timeout: 60000}).catch(function (error) {
+    await page.goto('http://localhost:8000/', {timeout: 60000}).catch(function (error) {
         throw new Error('TimeoutBrows');
     });;
     await record({
         browser,
         page,
         output: path.join(__dirname, `./resources/record.webm`),
-        fps: 25,
-        frames: 25 * 600,
+        fps: 1,
+        frames: 1 * 60,
         prepare: function () {}, // <-- add this line
         render: function () {} // <-- add this line
     });
 
-
-  
-    console.log('Dimensions:', dimensions);
-
-    // await browser.close();
+    await browser.close();
   })();
